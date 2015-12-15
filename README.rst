677947dba1abdf6683d7f32e8c1a929c80e7e625
Jared Messenger <jamessenger@gmail.com>
2015-12-15 14:59:35 -0800

    Fixing PyPI installation errors.

--- README.rst
+++ README.rst
@@ -3,7 +3,7 @@ oneID Connect
 oneID-connect is a Python authentication framework for the Internet of Things (IoT),
 servers and end-users. By sending messages with digital signatures, you can authenticate
 the origin of the message and ensure the message hasn't been tampered with.
-oneID-py makes it simple for projects that need to securely send messages and verify
+oneID-connect makes it simple for projects that need to securely send messages and verify
 the authentication of messages.
 
 For more information, please visit the docs.
---
+++ src/oneid/__about__.py
@@ -0,0 +1,19 @@
+__all__ = [
+    "__title__", "__summary__", "__uri__", "__version__", "__author__",
+    "__email__", "__license__", "__copyright__",
+]
+
+__title__ = "oneID-connect"
+__summary__ = ("oneID-connect is an identity & authentication "
+               "framework for the Internet of Things (IoT) "
+               "devices, servers, and end-users.")
+
+__uri__ = "https://github.com/OneID/oneID-connect-python"
+
+__version__ = "0.9.1"
+
+__author__ = "oneID Inc."
+__email__ = "support@oneID.com"
+
+__license__ = "Apache License, Version 2.0"
+__copyright__ = "Copyright 2012-2015 {0}".format(__author__)
