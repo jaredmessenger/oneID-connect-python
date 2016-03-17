a4f444324366b9e8435b24f5c503ca7fcb956774
Carl A Dunham <cdunham@gmail.com>
2016-03-17 12:44:47 -0700

    [FEATURE] Added load_pem_public_key

--- tests/test_keychain.py
+++ tests/test_keychain.py
@@ -12,11 +12,31 @@ class TestKeychain(unittest.TestCase):
         pem_path = os.path.join(self.x509_PATH, 'ec_sha256.pem')
         keychain.Keypair.from_secret_pem(path=pem_path)
 
+    def test_load_pem_path_pkcs8(self):
+        pem_path = os.path.join(self.x509_PATH, 'ec_pkcs8_private_key.pem')
+        keychain.Keypair.from_secret_pem(path=pem_path)
+
     def test_load_pem_bytes(self):
         pem_path = os.path.join(self.x509_PATH, 'ec_sha256.pem')
         with open(pem_path, 'rb') as f:
             pem_data = f.read()
             keychain.Keypair.from_secret_pem(key_bytes=pem_data)
+
+    def test_load_pem_bytes_pkcs8(self):
+        pem_path = os.path.join(self.x509_PATH, 'ec_pkcs8_private_key.pem')
+        with open(pem_path, 'rb') as f:
+            pem_data = f.read()
+            keychain.Keypair.from_secret_pem(key_bytes=pem_data)
+
+    def test_load_pem_public_path(self):
+        pem_path = os.path.join(self.x509_PATH, 'ec_public_key.pem')
+        keychain.Keypair.from_public_pem(path=pem_path)
+
+    def test_load_public_pem_bytes(self):
+        pem_path = os.path.join(self.x509_PATH, 'ec_public_key.pem')
+        with open(pem_path, 'rb') as f:
+            pem_data = f.read()
+            keychain.Keypair.from_public_pem(key_bytes=pem_data)
 
     def test_load_der_bytes(self):
         der_path = os.path.join(self.x509_PATH, 'ec_sha256.der')
